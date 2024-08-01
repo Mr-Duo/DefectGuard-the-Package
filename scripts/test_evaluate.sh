37d508a1e4cc059c9d9bf24cd83064b88196197f
Mr-Duo <duong.nd215336@sis.hust.edu.vn>
2024-08-01 15:04:00 +0700

    checkpoint 1-8-2024

--- scripts/test_evaluate.sh
+++ scripts/test_evaluate.sh
@@ -1,2 +1,3 @@
 #!bin/bash
-defectguard -debug evaluating -model lapredict -dg_save_folder . -repo_name javascript -repo_language JavaScript
+defectguard -debug evaluating \
+    -model lapredict -dg_save_folder . -repo_name javascript -repo_language JavaScript
--- scripts/test_train.sh
+++ scripts/test_train.sh
@@ -1,3 +1,5 @@
-#!bin/bash
-defectguard -debug training -model lr -dg_save_folder . -repo_name javascript -repo_language JavaScript;
-defectguard -debug evaluating -model lr -dg_save_folder . -repo_name javascript -repo_language JavaScript;
+#!/bin/bash
+defectguard -debug training -model simcom -dg_save_folder . -repo_name javascript -repo_language JavaScript 
+defectguard -debug evaluating -model simcom -dg_save_folder . -repo_name javascript -repo_language JavaScript 
+
+
