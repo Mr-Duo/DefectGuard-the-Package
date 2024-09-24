70e244923459e5204e03c6612ffaf3fe8749ce25
Mr-Duo <duong.nd215336@sis.hust.edu.vn>
2024-09-24 18:49:31 +0700

    more scripts

--- scripts/test_evaluate.sh
+++ scripts/test_evaluate.sh
@@ -1,9 +1,31 @@
-#!bin/bash
+#!/bin/bash
 
 defectguard evaluating \
     -model simcom \
-    -dg_save_folder . \
-    -repo_name FFmpeg \
-    -repo_language C \
     -feature_test_set "dataset/FFmpeg/feature/SETUP1-features-test-FFmpeg.jsonl" \
-    -commit_test_set "dataset/FFmpeg/commit/SETUP1-simcom-test-FFmpeg.jsonl"
+    -commit_test_set "dataset/FFmpeg/commit/SETUP1-simcom-test-FFmpeg.jsonl" \
+    -dictionary "dataset/FFmpeg/commit/dict-FFmpeg.jsonl" \
+    -dg_save_folder SETUP1 \
+    -repo_name FFmpeg \
+    -devive cuda \
+    -repo_language C
+
+defectguard evaluating \
+    -model simcom \
+    -feature_test_set "dataset/FFmpeg/feature/SETUP2-features-test-FFmpeg.jsonl" \
+    -commit_test_set "dataset/FFmpeg/commit/SETUP2-simcom-test-FFmpeg.jsonl" \
+    -dictionary "dataset/FFmpeg/commit/dict-FFmpeg.jsonl" \
+    -dg_save_folder SETUP2 \
+    -repo_name FFmpeg \
+    -devive cuda \
+    -repo_language C
+    
+defectguard evaluating \
+    -model simcom \
+    -feature_test_set "dataset/FFmpeg/feature/SETUP2-features-test-FFmpeg.jsonl" \
+    -commit_test_set "dataset/FFmpeg/commit/SETUP2-simcom-test-FFmpeg.jsonl" \
+    -dictionary "dataset/FFmpeg/commit/dict-FFmpeg.jsonl" \
+    -dg_save_folder SETUP3 \
+    -repo_name FFmpeg \
+    -devive cuda \
+    -repo_language C
