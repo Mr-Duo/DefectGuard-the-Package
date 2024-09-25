7024399529ea6a100d1e0bc3e4fcececb51aa037
Mr-Duo <duong.nd215336@sis.hust.edu.vn>
2024-09-26 06:39:21 +0700

    o

--- scripts/test_train.sh
+++ scripts/test_train.sh
@@ -2,11 +2,11 @@
 
 defectguard training \
     -model simcom \
-    -feature_train_set "dataset/FFmpeg/sampled/ros/feature/SETUP1-features-train-FFmpeg.jsonl" \
-    -commit_train_set "dataset/FFmpeg/sampled/ros/commit/SETUP1-simcom-train-FFmpeg.jsonl" \
+    -feature_train_set "dataset/FFmpeg/sampled/rus/feature/SETUP1-features-train-FFmpeg.jsonl" \
+    -commit_train_set "dataset/FFmpeg/sampled/rus/commit/SETUP1-simcom-train-FFmpeg.jsonl" \
     -commit_val_set "dataset/FFmpeg/commit/SETUP1-simcom-val-FFmpeg.jsonl" \
     -dictionary "dataset/FFmpeg/commit/dict-FFmpeg.jsonl" \
-    -dg_save_folder SETUP1 \
+    -dg_save_folder SETUP1/rus \
     -repo_name FFmpeg \
     -device cuda \
     -repo_language C  \
@@ -14,11 +14,11 @@ defectguard training \
 
 defectguard training \
     -model simcom \
-    -feature_train_set "dataset/FFmpeg/sampled/ros/feature/SETUP2-features-train-FFmpeg.jsonl" \
-    -commit_train_set "dataset/FFmpeg/sampled/ros/commit/SETUP2-simcom-train-FFmpeg.jsonl" \
+    -feature_train_set "dataset/FFmpeg/sampled/rus/feature/SETUP2-features-train-FFmpeg.jsonl" \
+    -commit_train_set "dataset/FFmpeg/sampled/rus/commit/SETUP2-simcom-train-FFmpeg.jsonl" \
     -commit_val_set "dataset/FFmpeg/commit/SETUP2-simcom-val-FFmpeg.jsonl" \
     -dictionary "dataset/FFmpeg/commit/dict-FFmpeg.jsonl" \
-    -dg_save_folder SETUP2 \
+    -dg_save_folder SETUP2/rus \
     -repo_name FFmpeg \
     -device cuda \
     -repo_language C \
@@ -26,11 +26,11 @@ defectguard training \
     
 defectguard training \
     -model simcom \
-    -feature_train_set "dataset/FFmpeg/sampled/ros/feature/SETUP3-features-train-FFmpeg.jsonl" \
-    -commit_train_set "dataset/FFmpeg/sampled/ros/commit/SETUP3-simcom-train-FFmpeg.jsonl" \
+    -feature_train_set "dataset/FFmpeg/sampled/rus/feature/SETUP3-features-train-FFmpeg.jsonl" \
+    -commit_train_set "dataset/FFmpeg/sampled/rus/commit/SETUP3-simcom-train-FFmpeg.jsonl" \
     -commit_val_set "dataset/FFmpeg/commit/SETUP3-simcom-val-FFmpeg.jsonl" \
     -dictionary "dataset/FFmpeg/commit/dict-FFmpeg.jsonl" \
-    -dg_save_folder SETUP3 \
+    -dg_save_folder SETUP3/rus \
     -repo_name FFmpeg \
     -device cuda \
     -repo_language C \
