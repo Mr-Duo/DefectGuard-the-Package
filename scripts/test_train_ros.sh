9c8549fadb0715490ee2fdc48ef8bec5c79c4535
Mr-Duo <duong.nd215336@sis.hust.edu.vn>
2024-10-03 16:49:54 +0700

    b

--- scripts/test_train_ros.sh
+++ scripts/test_train_ros.sh
@@ -35,3 +35,33 @@ defectguard training \
     -device cuda \
     -repo_language C \
     -epoch 30
+
+defectguard evaluating \
+    -model simcom \
+    -feature_test_set "dataset/FFmpeg/SETUP1/SETUP1-FFmpeg-features-test.jsonl" \
+    -commit_test_set "dataset/FFmpeg/SETUP1/SETUP1-FFmpeg-simcom-val.jsonl" \
+    -dictionary "dataset/FFmpeg/SETUP1/dict-FFmpeg.jsonl" \
+    -dg_save_folder SETUP1/ros \
+    -repo_name FFmpeg \
+    -device cuda \
+    -repo_language C
+
+defectguard evaluating \
+    -model simcom \
+    -feature_test_set "dataset/FFmpeg/SETUP2/SETUP2-FFmpeg-features-test.jsonl" \
+    -commit_test_set "dataset/FFmpeg/SETUP2/SETUP2-FFmpeg-simcom-val.jsonl" \
+    -dictionary "dataset/FFmpeg/SETUP2/dict-FFmpeg.jsonl" \
+    -dg_save_folder SETUP2/ros \
+    -repo_name FFmpeg \
+    -device cuda \
+    -repo_language C
+
+defectguard evaluating \
+    -model simcom \
+    -feature_test_set "dataset/FFmpeg/SETUP3/SETUP3-FFmpeg-features-test.jsonl" \
+    -commit_test_set "dataset/FFmpeg/SETUP3/SETUP3-FFmpeg-simcom-val.jsonl" \
+    -dictionary "dataset/FFmpeg/SETUP3/dict-FFmpeg.jsonl" \
+    -dg_save_folder SETUP3/ros \
+    -repo_name FFmpeg \
+    -device cuda \
+    -repo_language C
