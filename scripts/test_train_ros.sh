435a9db2e18c8828a5362a8fc30d332bc49ccee0
Mr-Duo <duong.nd215336@sis.hust.edu.vn>
2024-10-05 14:05:40 +0700

    a

--- scripts/test_train_ros.sh
+++ scripts/test_train_ros.sh
@@ -27,13 +27,55 @@ mkdir SETUP1/ros SETUP2/ros SETUP3/ros SETUP4/ros SETUP5/ros
 #     -repo_language C \
 #     -epoch 30
     
+python3 -m defectguard.cli training  \
+    -model simcom \
+    -feature_train_set "dataset/FFmpeg/SETUP3/ros/SETUP3-FFmpeg-features-train.jsonl" \
+    -commit_train_set "dataset/FFmpeg/SETUP3/ros/SETUP3-FFmpeg-simcom-train.jsonl" \
+    -commit_val_set "dataset/FFmpeg/SETUP3/SETUP3-FFmpeg-simcom-val.jsonl" \
+    -dictionary "dataset/FFmpeg/SETUP2/dict-FFmpeg.jsonl" \
+    -dg_save_folder SETUP3/ros \
+    -repo_name FFmpeg \
+    -device cuda \
+    -repo_language C \
+    -epoch 30
+
+python3 -m defectguard.cli evaluating \
+    -model simcom \
+    -feature_test_set "dataset/FFmpeg/SETUP1/SETUP1-FFmpeg-features-test.jsonl" \
+    -commit_test_set "dataset/FFmpeg/SETUP1/SETUP1-FFmpeg-simcom-val.jsonl" \
+    -dictionary "dataset/FFmpeg/SETUP1/dict-FFmpeg.jsonl" \
+    -dg_save_folder SETUP1/ros \
+    -repo_name FFmpeg \
+    -device cuda \
+    -repo_language C
+
+python3 -m defectguard.cli evaluating \
+    -model simcom \
+    -feature_test_set "dataset/FFmpeg/SETUP2/SETUP2-FFmpeg-features-test.jsonl" \
+    -commit_test_set "dataset/FFmpeg/SETUP2/SETUP2-FFmpeg-simcom-val.jsonl" \
+    -dictionary "dataset/FFmpeg/SETUP2/dict-FFmpeg.jsonl" \
+    -dg_save_folder SETUP2/ros \
+    -repo_name FFmpeg \
+    -device cuda \
+    -repo_language C
+
+python3 -m defectguard.cli evaluating \
+    -model simcom \
+    -feature_test_set "dataset/FFmpeg/SETUP3/SETUP3-FFmpeg-features-test.jsonl" \
+    -commit_test_set "dataset/FFmpeg/SETUP3/SETUP3-FFmpeg-simcom-val.jsonl" \
+    -dictionary "dataset/FFmpeg/SETUP3/dict-FFmpeg.jsonl" \
+    -dg_save_folder SETUP3/ros \
+    -repo_name FFmpeg \
+    -device cuda \
+    -repo_language C
+
 # python3 -m defectguard.cli training  \
 #     -model simcom \
-#     -feature_train_set "dataset/FFmpeg/SETUP3/ros/SETUP3-FFmpeg-features-train.jsonl" \
-#     -commit_train_set "dataset/FFmpeg/SETUP3/ros/SETUP3-FFmpeg-simcom-train.jsonl" \
-#     -commit_val_set "dataset/FFmpeg/SETUP3/SETUP3-FFmpeg-simcom-val.jsonl" \
+#     -feature_train_set "dataset/FFmpeg/SETUP4/ros/SETUP4-FFmpeg-features-train.jsonl" \
+#     -commit_train_set "dataset/FFmpeg/SETUP4/ros/SETUP4-FFmpeg-simcom-train.jsonl" \
+#     -commit_val_set "dataset/FFmpeg/SETUP4/SETUP4-FFmpeg-simcom-val.jsonl" \
 #     -dictionary "dataset/FFmpeg/SETUP2/dict-FFmpeg.jsonl" \
-#     -dg_save_folder SETUP3/ros \
+#     -dg_save_folder SETUP4/ros \
 #     -repo_name FFmpeg \
 #     -device cuda \
 #     -repo_language C \
@@ -41,74 +83,32 @@ mkdir SETUP1/ros SETUP2/ros SETUP3/ros SETUP4/ros SETUP5/ros
 
 # python3 -m defectguard.cli evaluating \
 #     -model simcom \
-#     -feature_test_set "dataset/FFmpeg/SETUP1/SETUP1-FFmpeg-features-test.jsonl" \
-#     -commit_test_set "dataset/FFmpeg/SETUP1/SETUP1-FFmpeg-simcom-val.jsonl" \
-#     -dictionary "dataset/FFmpeg/SETUP1/dict-FFmpeg.jsonl" \
-#     -dg_save_folder SETUP1/ros \
+#     -feature_test_set "dataset/FFmpeg/SETUP4/SETUP4-FFmpeg-features-test.jsonl" \
+#     -commit_test_set "dataset/FFmpeg/SETUP4/SETUP4-FFmpeg-simcom-val.jsonl" \
+#     -dictionary "dataset/FFmpeg/SETUP4/dict-FFmpeg.jsonl" \
+#     -dg_save_folder SETUP4/ros \
 #     -repo_name FFmpeg \
 #     -device cuda \
 #     -repo_language C
 
-# python3 -m defectguard.cli evaluating \
+# python3 -m defectguard.cli training  \
 #     -model simcom \
-#     -feature_test_set "dataset/FFmpeg/SETUP2/SETUP2-FFmpeg-features-test.jsonl" \
-#     -commit_test_set "dataset/FFmpeg/SETUP2/SETUP2-FFmpeg-simcom-val.jsonl" \
+#     -feature_train_set "dataset/FFmpeg/SETUP5/ros/SETUP5-FFmpeg-features-train.jsonl" \
+#     -commit_train_set "dataset/FFmpeg/SETUP5/ros/SETUP5-FFmpeg-simcom-train.jsonl" \
+#     -commit_val_set "dataset/FFmpeg/SETUP5/SETUP5-FFmpeg-simcom-val.jsonl" \
 #     -dictionary "dataset/FFmpeg/SETUP2/dict-FFmpeg.jsonl" \
-#     -dg_save_folder SETUP2/ros \
+#     -dg_save_folder SETUP5/ros \
 #     -repo_name FFmpeg \
 #     -device cuda \
-#     -repo_language C
+#     -repo_language C \
+#     -epoch 30
 
 # python3 -m defectguard.cli evaluating \
 #     -model simcom \
-#     -feature_test_set "dataset/FFmpeg/SETUP3/SETUP3-FFmpeg-features-test.jsonl" \
-#     -commit_test_set "dataset/FFmpeg/SETUP3/SETUP3-FFmpeg-simcom-val.jsonl" \
-#     -dictionary "dataset/FFmpeg/SETUP3/dict-FFmpeg.jsonl" \
-#     -dg_save_folder SETUP3/ros \
+#     -feature_test_set "dataset/FFmpeg/SETUP5/SETUP5-FFmpeg-features-test.jsonl" \
+#     -commit_test_set "dataset/FFmpeg/SETUP5/SETUP5-FFmpeg-simcom-val.jsonl" \
+#     -dictionary "dataset/FFmpeg/SETUP5/dict-FFmpeg.jsonl" \
+#     -dg_save_folder SETUP5/ros \
 #     -repo_name FFmpeg \
 #     -device cuda \
 #     -repo_language C
-
-python3 -m defectguard.cli training  \
-    -model simcom \
-    -feature_train_set "dataset/FFmpeg/SETUP4/ros/SETUP4-FFmpeg-features-train.jsonl" \
-    -commit_train_set "dataset/FFmpeg/SETUP4/ros/SETUP4-FFmpeg-simcom-train.jsonl" \
-    -commit_val_set "dataset/FFmpeg/SETUP4/SETUP4-FFmpeg-simcom-val.jsonl" \
-    -dictionary "dataset/FFmpeg/SETUP2/dict-FFmpeg.jsonl" \
-    -dg_save_folder SETUP4/ros \
-    -repo_name FFmpeg \
-    -device cuda \
-    -repo_language C \
-    -epoch 30
-
-python3 -m defectguard.cli evaluating \
-    -model simcom \
-    -feature_test_set "dataset/FFmpeg/SETUP4/SETUP4-FFmpeg-features-test.jsonl" \
-    -commit_test_set "dataset/FFmpeg/SETUP4/SETUP4-FFmpeg-simcom-val.jsonl" \
-    -dictionary "dataset/FFmpeg/SETUP4/dict-FFmpeg.jsonl" \
-    -dg_save_folder SETUP4/ros \
-    -repo_name FFmpeg \
-    -device cuda \
-    -repo_language C
-
-python3 -m defectguard.cli training  \
-    -model simcom \
-    -feature_train_set "dataset/FFmpeg/SETUP5/ros/SETUP5-FFmpeg-features-train.jsonl" \
-    -commit_train_set "dataset/FFmpeg/SETUP5/ros/SETUP5-FFmpeg-simcom-train.jsonl" \
-    -commit_val_set "dataset/FFmpeg/SETUP5/SETUP5-FFmpeg-simcom-val.jsonl" \
-    -dictionary "dataset/FFmpeg/SETUP2/dict-FFmpeg.jsonl" \
-    -dg_save_folder SETUP5/ros \
-    -repo_name FFmpeg \
-    -device cuda \
-    -repo_language C \
-    -epoch 30
-
-python3 -m defectguard.cli evaluating \
-    -model simcom \
-    -feature_test_set "dataset/FFmpeg/SETUP5/SETUP5-FFmpeg-features-test.jsonl" \
-    -commit_test_set "dataset/FFmpeg/SETUP5/SETUP5-FFmpeg-simcom-val.jsonl" \
-    -dictionary "dataset/FFmpeg/SETUP5/dict-FFmpeg.jsonl" \
-    -dg_save_folder SETUP5/ros \
-    -repo_name FFmpeg \
-    -device cuda \
-    -repo_language C
