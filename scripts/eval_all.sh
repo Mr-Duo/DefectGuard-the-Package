c87484ea40365c66fb71f36446e224cc04696bc8
Mr-Duo <duong.nd215336@sis.hust.edu.vn>
2024-10-07 00:23:37 +0700

    a

--- scripts/eval_all.sh
+++ scripts/eval_all.sh
@@ -11,15 +11,17 @@ for i in "${sampling[@]}"; do
     for j in "${setup[@]}"; do
         for k in "${model[@]}"; do
             echo "Train $k $j with $i"
-            python3 -m defectguard.cli training \
+            python3 -m defectguard.cli training  \
                 -model $k \
-                -feature_test_set "dataset/FFmpeg/$j/$j-FFmpeg-features-test.jsonl" \
-                -commit_test_set "dataset/FFmpeg/$j/$j-FFmpeg-simcom-test.jsonl" \
-                -dictionary "dataset/FFmpeg/SETUP1/dict-FFmpeg.jsonl" \
+                -feature_train_set "dataset/FFmpeg/$j/$i/$j-FFmpeg-features-train.jsonl" \
+                -commit_train_set "dataset/FFmpeg/$j/$i/$j-FFmpeg-simcom-train.jsonl" \
+                -commit_val_set "dataset/FFmpeg/$j/$j-FFmpeg-simcom-val.jsonl" \
+                -dictionary "dataset/FFmpeg/SETUP2/dict-FFmpeg.jsonl" \
                 -dg_save_folder $j/$i \
                 -repo_name FFmpeg \
                 -device cuda \
-                -repo_language C
+                -repo_language C \
+                -epoch 30
 
             echo "Eval $k $j with $i"
             python3 -m defectguard.cli evaluating \
