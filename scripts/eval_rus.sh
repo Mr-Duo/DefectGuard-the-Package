c38011fcf7c4d5e2ed7e0fc24e247051ae4667ef
Mr-Duo <duong.nd215336@sis.hust.edu.vn>
2024-12-11 10:05:10 +0700

    a

--- scripts/eval_rus.sh
+++ scripts/eval_rus.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
-
+$project="FFmpeg"
+$setup="SETUP1"
 python3 -m defectguard.cli evaluating \
     -model simcom \
-    -feature_test_set "dataset/FFmpeg/SETUP1/SETUP1-FFmpeg-features-test.jsonl" \
-    -commit_test_set "dataset/FFmpeg/SETUP1/SETUP1-FFmpeg-simcom-val.jsonl" \
-    -dictionary "dataset/FFmpeg/SETUP1/dict-FFmpeg.jsonl" \
-    -dg_save_folder SETUP1/rus \
-    -repo_name FFmpeg \
+    -feature_test_set "dataset/$project/$setup/$setup-$project-features-test.jsonl" \
+    -commit_test_set "dataset/$project/$setup/$setup-$project-simcom-test.jsonl" \
+    -dictionary "dataset/$project/$setup/dict-$project.jsonl" \
+    -dg_save_folder $setup/rus \
+    -repo_name $project \
     -device cuda \
     -repo_language C
 
-python3 -m defectguard.cli evaluating \
-    -model simcom \
-    -feature_test_set "dataset/FFmpeg/SETUP2/SETUP2-FFmpeg-features-test.jsonl" \
-    -commit_test_set "dataset/FFmpeg/SETUP2/SETUP2-FFmpeg-simcom-val.jsonl" \
-    -dictionary "dataset/FFmpeg/SETUP2/dict-FFmpeg.jsonl" \
-    -dg_save_folder SETUP2/rus \
-    -repo_name FFmpeg \
-    -device cuda \
-    -repo_language C
+# python3 -m defectguard.cli evaluating \
+#     -model simcom \
+#     -feature_test_set "dataset/FFmpeg/SETUP2/SETUP2-FFmpeg-features-test.jsonl" \
+#     -commit_test_set "dataset/FFmpeg/SETUP2/SETUP2-FFmpeg-simcom-val.jsonl" \
+#     -dictionary "dataset/FFmpeg/SETUP2/dict-FFmpeg.jsonl" \
+#     -dg_save_folder SETUP2/rus \
+#     -repo_name FFmpeg \
+#     -device cuda \
+#     -repo_language C
 
-python3 -m defectguard.cli evaluating \
-    -model simcom \
-    -feature_test_set "dataset/FFmpeg/SETUP3/SETUP3-FFmpeg-features-test.jsonl" \
-    -commit_test_set "dataset/FFmpeg/SETUP3/SETUP3-FFmpeg-simcom-val.jsonl" \
-    -dictionary "dataset/FFmpeg/SETUP3/dict-FFmpeg.jsonl" \
-    -dg_save_folder SETUP3/rus \
-    -repo_name FFmpeg \
-    -device cuda \
-    -repo_language C
+# python3 -m defectguard.cli evaluating \
+#     -model simcom \
+#     -feature_test_set "dataset/FFmpeg/SETUP3/SETUP3-FFmpeg-features-test.jsonl" \
+#     -commit_test_set "dataset/FFmpeg/SETUP3/SETUP3-FFmpeg-simcom-val.jsonl" \
+#     -dictionary "dataset/FFmpeg/SETUP3/dict-FFmpeg.jsonl" \
+#     -dg_save_folder SETUP3/rus \
+#     -repo_name FFmpeg \
+#     -device cuda \
+#     -repo_language C
